50b35947ddb042690052a8068da4dc031eff276f
DoganHaluk <ha_look@yahoo.com>
2022-05-13 17:50:18 +0200

    post integration test added

--- src/test/java/be/vdab/restservice/restcontrollers/FiliaalControllerTest.java
+++ src/test/java/be/vdab/restservice/restcontrollers/FiliaalControllerTest.java
@@ -3,11 +3,13 @@ package be.vdab.restservice.restcontrollers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -37,5 +39,14 @@ class FiliaalControllerTest extends AbstractTransactionalJUnit4SpringContextTest
         mvc.perform(get("/filialen/{id}", id))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("id").value(id));
+    }
+
+    @Test
+    void nieuweFiliaalToevoegen() throws Exception{
+        String jsonBody="{\"naam\":\"Filiaal\", \"gemeente\":\"Gemeente\", \"omzet\":1}";
+        mvc.perform(post("/filialen")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonBody))
+                .andExpect(status().isCreated());
     }
 }
